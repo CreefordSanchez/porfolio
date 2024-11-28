07a63667285412281761a876459af413cbe21e74
Creeford Sanchez <creewrk@gmail.com>
2024-11-27 19:44:21 -0800

    'implement-menu-button'

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,3 +2,10 @@
 
 import { listener, selector } from "./data/utitily.js";
 
+const menuBtn = selector('.fa-bars');
+const menuTab = selector('.menu-tab');
+
+listener(menuBtn, 'click', () => {
+  menuTab.classList.toggle('menu-display');
+  menuBtn.classList.toggle('menu-btn-toogle');
+});
